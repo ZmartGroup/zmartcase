fc19fc6a2322d1ce86ba12f55950e13e834165f5
Johan Lundgren <johan.emil.lundgren@gmail.com>
2015-04-22 10:42:31 +0200

    added sorcery core and regenerated user via sorcery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name
-  has_many :cases
-
-  def name
-    first_name + " " + last_name
-  end
-
+  authenticates_with_sorcery!
+  # attr_accessible :title, :body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150416145138) do
+ActiveRecord::Schema.define(:version => 20150422083900) do
 
   create_table "cases", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -42,11 +42,15 @@ ActiveRecord::Schema.define(:version => 20150416145138) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "email",            :null => false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
