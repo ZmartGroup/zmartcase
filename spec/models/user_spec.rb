8345afc5475de3a6021a6294af5fe2d966e3f5a7
Johan Lundgren <johan.emil.lundgren@gmail.com>
2015-04-23 11:25:19 +0200

    first test added for user name method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 
 describe User do
 
-  it "is named Kalle"
+  it "is named Kalle" do
+    user = User.new
+    user.first_name = "Kalle"
+    user.last_name = "testsson"
+    user.name.should == "Kalle testsson"
+  end
 
 end
