c3c03bed5e31ad5a98ed24d69066e34b336e76d0
Johan Lundgren <johan.emil.lundgren@gmail.com>
2015-04-23 15:22:33 +0200

    added test for user attribute validations

--- Gemfile
+++ Gemfile
@@ -21,8 +21,10 @@ group :assets do
 end
 
 gem 'jquery-rails'
-gem 'rspec'
-gem 'rspec-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,47 @@ require 'rails_helper'
 
 
 describe User do
+  user = User.new
+  user.first_name = "Kalle"
+  user.last_name = "testsson"
 
   it "is named Kalle" do
-    user = User.new
-    user.first_name = "Kalle"
-    user.last_name = "testsson"
     expect(user.name).to eq("Kalle testsson")
+  end
+
+  it "should not be valid user" do
+    expect(user).to_not be_valid
+  end
+
+  it "should now be a valid user" do
+    user.email = "kalle@zc.se"
+    user.password = user.password_confirmation = "123"
+    expect(user).to be_valid
+  end
+
+  it "does not allow no password" do
+    user1 = User.create(
+      first_name: "nisse",
+      last_name: "testfeldt",
+      email: "nisse@test.se")
+    expect(user1.errors[:password]).to include("can't be blank")
+  end
+
+
+  it "does not allow duplicated emails" do
+    User.create(
+      first_name: "arne",
+      last_name: "testgren",
+      password: "arne123",
+      password_confirmation: "arne123",
+      email: "testmail@test.se")
+    user2 = User.create(
+      first_name: "bengt",
+      last_name: "testberg",
+      password: "bengan123",
+      password_confirmation: "bengan123",
+      email: "testmail@test.se")
+    expect(user2.errors[:email]).to include("has already been taken")
   end
 
 end
