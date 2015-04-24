fe5e586fc1e1b71eaccfacb84cdba49a3a47edb7
OlSom <oliver.somos@gmail.com>
2015-04-24 10:29:58 +0200

    tests for ZmartMailer, EmailAccount

--- app/mailers/zmart_mailer.rb
+++ app/mailers/zmart_mailer.rb
@@ -3,6 +3,7 @@ require 'mail'
 
 class ZmartMailer < ActionMailer::Base
 	def self.fetch_all_from_all
+		Email.delete_all
 		emails = Array.new
 		EmailAccount.all.each do |acc|
 			emails.push(*fetch_all_from_one(acc))
@@ -19,7 +20,7 @@ class ZmartMailer < ActionMailer::Base
 	end
   	
 	def self.fetch_all_from_one(email_account)
-		#emails = Array.new
+		emails = Array.new
 		imap = Net::IMAP.new(email_account.imap, {:port => email_account.port, :ssl => email_account.enable_ssl})
 		imap.login(email_account.user_name, email_account.password)
 		imap.select('INBOX')
@@ -27,19 +28,18 @@ class ZmartMailer < ActionMailer::Base
 			imap.store(message_id, '+FLAGS', [:Seen])
 			msg = imap.fetch(message_id, 'RFC822')[0].attr['RFC822']
 			mail = Mail.new(msg)
-			Email.create(date: mail.date, to: mail.to, from: mail.from, subject: mail.subject, body: mail.body)
-
-			#ste = Email.new(date: mail.date, to: mail.to, from: mail.from, subject: mail.subject, body: mail.body)
-			#emails.push ste
+			ste = Email.new(date: mail.date, to: mail.to, from: mail.from, subject: mail.subject, body: mail.body)
+			ste.save
+			emails.push ste
 		end
 		imap.logout()
 		imap.disconnect() 	
-		#return emails
+		return emails
 	end
 
 
 	def self.fetch_new_from_one(email_account)
-		#emails = Array.new
+		emails = Array.new
 		imap = Net::IMAP.new(email_account.imap, {:port => email_account.port, :ssl => email_account.enable_ssl})
 		imap.login(email_account.user_name, email_account.password)
 		imap.select('INBOX')
@@ -47,13 +47,12 @@ class ZmartMailer < ActionMailer::Base
 			imap.store(message_id, '+FLAGS', [:Seen])
 			msg = imap.fetch(message_id, 'RFC822')[0].attr['RFC822']
 			mail = Mail.new(msg)
-			Email.create(date: mail.date, to: mail.to, from: mail.from, subject: mail.subject, body: mail.body)
-
-			#ste = Email.new(date: mail.date, to: mail.to, from: mail.from, subject: mail.subject, body: mail.body)
-			#emails.push ste
+			ste = Email.new(date: mail.date, to: mail.to, from: mail.from, subject: mail.subject, body: mail.body)
+			ste.save
+			emails.push ste
 		end
 		imap.logout()
 		imap.disconnect() 
-		#return emails
+		return emails
 	end
 end
--- spec/models/email_account_spec.rb
+++ spec/models/email_account_spec.rb
@@ -1,1 +1,10 @@
 require 'rails_helper'
+
+describe EmailAccount do
+	ea = EmailAccount.new(imap:"imap.gmail.com", port: "993", enable_ssl: true, user_name: "barasparaprojtest@gmail.com", password: "Kth2015!")
+
+
+	it "imap should work" do
+  		expect(ea.imap).to eq("imap.gmail.com")
+  	end
+end
