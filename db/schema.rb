eb6e954d8ca9be336957591d223b2038cdf8fcfb
Johan Lundgren <johan.emil.lundgren@gmail.com>
2015-04-24 12:08:05 +0200

    migration reset

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(:version => 20150423120448) do
     t.integer  "case_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.integer  "type_id"
+    t.datetime "date"
+    t.string   "to"
+    t.string   "from"
     t.integer  "category_id"
   end
 
