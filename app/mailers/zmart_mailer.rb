27a892478a52dc7a34f0c1a27a14c35041f17102
Johan Lundgren <johan.emil.lundgren@gmail.com>
2015-04-28 11:07:02 +0200

    added modal for new email, new note and priority for cases

--- app/mailers/zmart_mailer.rb
+++ app/mailers/zmart_mailer.rb
@@ -61,4 +61,24 @@ class ZmartMailer < ActionMailer::Base
 		imap.disconnect()
 		return emails
 	end
+
+	def self.fetch_from_one(email_account, only_new: false)
+    inbox_type = only_new ? "UNSEEN" : "ALL"
+		emails = Array.new
+		imap = Net::IMAP.new(email_account.imap, {:port => email_account.port, :ssl => email_account.enable_ssl})
+		imap.login(email_account.user_name, email_account.password)
+		imap.select('INBOX')
+		imap.search([inbox_type]).reverse_each do |message_id|
+			imap.store(message_id, '+FLAGS', [:Seen])
+			msg = imap.fetch(message_id, 'RFC822')[0].attr['RFC822']
+			mail = Mail.new(msg)
+			ste = Email.new(date: mail.date, to: mail.to, from: mail.from, subject: mail.subject, body: mail.body)
+			ste.save
+			emails.push ste
+		end
+		imap.logout()
+		imap.disconnect()
+		return emails
+	end
+
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,5 @@
 class Email < ActiveRecord::Base
-  attr_accessible :from, :to, :date, :body, :case_id, :subject
   belongs_to :case
   belongs_to :type
-  belongs_to :category
-  
+
 end
