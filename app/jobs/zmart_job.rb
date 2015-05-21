3487c754065868478190fff4f0add8a357a562d2
OlSom <oliver.somos@gmail.com>
2015-05-21 16:18:18 +0200

    background email sending

---
+++ app/jobs/zmart_job.rb
@@ -0,0 +1,17 @@
+class ZmartJob
+	include SuckerPunch::Job
+
+	def perform(email, attach)
+		ActiveRecord::Base.connection_pool.with_connection do
+			mail = MailSender.create_email(email)
+	    unless attach[].nil?
+	      attach[].each do |attachment|
+	        mail.attachments[attachment.original_filename] = File.read(attachment.path)
+	      end
+	    end
+	    mail.deliver
+      email.raw = MailCompressor.compress_mail(mail)
+      email.save
+		end
+	end
+end
